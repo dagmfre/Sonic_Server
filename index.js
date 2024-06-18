3a78f9a2a79c6d142d5dd3b0998b9bedeffda431
dagmfre <dagmfre@gmail.com>
2024-06-18 03:14:34 +0300

    Rename app.js to index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors(corsOptions));
+
+const artistRoutes = require("./Routes/artistRoutes");
+const topArtists = require("./Routes/topArtists");
+const songRoutes = require("./Routes/songRoutes");
+const fileRoutes = require("./Routes/fileRoutes");
+
+app.use("/artist", artistRoutes);
+app.use("/topArtists", topArtists);
+app.use("/songs", songRoutes);
+app.use("/file", fileRoutes);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
