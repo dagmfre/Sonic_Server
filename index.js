396525fbc72129da3f4e9f2409adf479ade2bec9
dagmfre <dagmfre@gmail.com>
2024-06-18 15:39:39 +0300

    Define tracks route

--- index.js
+++ index.js
@@ -11,10 +11,12 @@ const artistRoutes = require("./Routes/artistRoutes");
 const topArtists = require("./Routes/topArtists");
 const songRoutes = require("./Routes/songRoutes");
 const fileRoutes = require("./Routes/fileRoutes");
+const tracksRoute = require("./Routes/tracksRoute");
 
 app.use("/api/artists", artistRoutes);
 app.use("/api/topArtists", topArtists);
 app.use("/api/songs", songRoutes);
+app.use("/api/tracks", tracksRoute);
 app.use("/file", fileRoutes);
 
 const PORT = process.env.PORT || 3001;
