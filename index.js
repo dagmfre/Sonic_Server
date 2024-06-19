2eb570679b48b496b763eba255847caa89a70cdb
dagmfre <dagmfre@gmail.com>
2024-06-19 08:58:53 +0300

    Place globalErrorHandler inside /Utils folder

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const globalErrorHandler = require("./Middlewares/globalErrorHandler");
+const globalErrorHandler = require("./Utils/globalErrorHandler");
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
