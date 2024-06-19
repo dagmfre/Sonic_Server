2ad1ee82a15579e877ea11f4d81978e18f4839a5
dagmfre <dagmfre@gmail.com>
2024-06-19 04:02:58 +0300

    Add validation and error handling

--- Routes/songRoutes.js
+++ Routes/songRoutes.js
@@ -1,8 +1,25 @@
 const express = require("express");
 const { songUploader } = require("../Controllers/songController");
 const upload = require("../Config/multer");
+const { validateSong } = require("../Validations/validate");
+const { validationResult } = require("express-validator");
 
 const router = express.Router();
-router.post("/", upload.fields([{ name: "image" }, { name: "song" }]), songUploader);
+
+router.post(
+  "/",
+  upload.fields([{ name: "image" }, { name: "song" }]),
+  validateSong,
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        errors: errors.array(),
+      });
+    }
+    next();
+  },
+  songUploader
+);
 
 module.exports = router;
