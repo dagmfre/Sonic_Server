8c4aaeff78a4e46b3765b2aa21431276d0ab4080
dagmfre <dagmfre@gmail.com>
2024-06-20 15:51:46 +0300

    Update static and method

--- Models/Users/methods.js
+++ Models/Users/methods.js
@@ -1,7 +1,5 @@
-const saveRegisteredUser = async function (userData) {
-    return await this.save(userData);
-  };
-  
-  module.exports = {
-    saveRegisteredUser
-  };
+const saveRegisteredUser = async function () {
+  return await this.save();
+};
+
+module.exports = { saveRegisteredUser };
