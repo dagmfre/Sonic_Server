36a132ea286db2f293d64f5f6267ac81308dcbfb
dagmfre <dagmfre@gmail.com>
2024-06-20 15:52:54 +0300

    Remove password hashing from uderModel

--- Models/Users/userModel.js
+++ Models/Users/userModel.js
@@ -1,13 +1,4 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
 const userSchema = require("./userSchema");
-
-// Hash the password before saving the user model
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
-    this.password = await bcrypt.hash(this.password, 12);
-  }
-  next();
-});
 
 module.exports = mongoose.model("User", userSchema);
