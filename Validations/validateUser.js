7e5ca83faa4d11d03b1051bcb35ab0a556aa5b6c
dagmfre <dagmfre@gmail.com>
2024-06-20 15:55:29 +0300

    create user auth validator and change validator name

---
+++ Validations/validateUser.js
@@ -0,0 +1,28 @@
+const { body } = require("express-validator");
+
+const emailValidator = body("email")
+  .notEmpty()
+  .withMessage("Title should not be empty")
+  .isEmail()
+  .withMessage("Please enter a valid email address.")
+  .normalizeEmail();
+
+const usernameValidator = body("username")
+  .notEmpty()
+  .withMessage("Username should not be empty")
+  .isLength({ min: 4, max: 20 })
+  .withMessage("Username must be between 4 and 20 characters long.");
+
+const passwordValidator = body("password")
+  .notEmpty()
+  .withMessage("Singer should not be empty")
+  .isAlphanumeric("en-US", { ignore: ["-", "."] })
+  .withMessage(
+    "Username can only contain letters, numbers, hyphens, and periods."
+  );
+
+const validateSong = [emailValidator, usernameValidator, passwordValidator];
+
+module.exports = {
+  validateUser,
+};
