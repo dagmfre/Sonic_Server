1c1f22d422ccb1d2511cd0e8f01df987e5a0bbe1
dagmfre <dagmfre@gmail.com>
2024-06-20 15:48:30 +0300

    Parse token and and use middleware

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const cookieParser = require("cookie-parser");
 const globalErrorHandler = require("./Utils/globalErrorHandler");
+const isAuthenticated = require("./Utils/authMiddleware");
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 const artistRoutes = require("./Routes/artistRoutes");
 const topArtists = require("./Routes/topArtists");
@@ -16,13 +19,14 @@ const tracksRoute = require("./Routes/tracksRoute");
 const loginRoute = require("./Routes/loginRoutes");
 const signupRoute = require("./Routes/signupRoutes");
 
+app.use("/login", loginRoute);
+app.use("/register", signupRoute);
+app.use(isAuthenticated);
 app.use("/api/artists", artistRoutes);
 app.use("/api/topArtists", topArtists);
 app.use("/api/tracks", tracksRoute);
 app.use("/api/songs", songRoutes);
 app.use("/file", fileRoutes);
-app.use("/login", loginRoute);
-app.use("/register", signupRoute);
 
 // Global error handler middleware
 app.use(globalErrorHandler);
