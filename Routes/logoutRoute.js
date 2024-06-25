b27cc8df629be10ddf6716068c058d54be4c1dc7
dagmfre <dagmfre@gmail.com>
2024-06-25 09:55:29 +0300

    Remove logout route

--- Routes/logoutRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const logoutController = require("../Controllers/logoutController");
-
-const router = express.Router();
-
-router.get("/logout", logoutController);
-
-module.exports = router;
