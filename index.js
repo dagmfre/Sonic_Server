c6afb7eb1c943f1c867ca3361039f5a8e188b09b
dagmfre <dagmfre@gmail.com>
2024-06-25 09:47:30 +0300

    Rfactor index.js

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-const express = require("express");
-const cors = require("cors");
+import express from "express";
+import cors from "cors";
 const app = express();
-const cookieParser = require("cookie-parser");
-const globalErrorHandler = require("./Utils/globalErrorHandler");
-const isAuthenticated = require("./Utils/authMiddleware");
-require("dotenv").config();
+import globalErrorHandler from "./Utils/globalErrorHandler.js";
+import isAuthenticated from "./Utils/authMiddleware.js";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import { admin, adminRouter } from "./config/admin.js";
 
-// Middleware setup
+dotenv.config();
+
+// Middleware setupz
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
+app.use(admin.options.rootPath, adminRouter);
 
 // Importing route handlers
-const artistRoutes = require("./Routes/artistRoutes");
-const topArtists = require("./Routes/topArtists");
-const songRoutes = require("./Routes/songRoutes");
-const fileRoutes = require("./Routes/fileRoutes");
-const tracksRoute = require("./Routes/tracksRoute");
-const loginRoute = require("./Routes/loginRoutes");
-const signupRoute = require("./Routes/signupRoutes");
-const logoutRoute = require("./Routes/logoutRoute");
+import artistRoutes from "./Routes/artistRoutes.js";
+import topArtists from "./Routes/topArtists.js";
+import songRoutes from "./Routes/songRoutes.js";
+import fileRoutes from "./Routes/fileRoutes.js";
+import tracksRoute from "./Routes/tracksRoute.js";
+import loginRoute from "./Routes/loginRoutes.js";
+import signupRoute from "./Routes/signupRoutes.js";
 
 // Publicly accessible routes
 app.use("/login", loginRoute);
 app.use("/register", signupRoute);
-app.use("/logout", logoutRoute);
 
 // Authentication middleware and protected routes
 app.use(isAuthenticated);
