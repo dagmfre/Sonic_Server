29c55a4d4dea3e8e35d7625d82e2abdc3ddf01bf
dagmfre <dagmfre@gmail.com>
2024-12-27 00:34:39 +0300

    Refactor isOwner function to improve error handling and ensure file ownership verification

--- Routes/fileRoutes.js
+++ Routes/fileRoutes.js
@@ -4,21 +4,33 @@ import { getFile, deleteFile } from "../Controllers/fileController.js";
 import User from "../Models/Users/userModel.js";
 
 const router = express.Router();
+
 const isOwner = async (filename, sender) => {
-  // Find a user who owns the file, either audio or image
-  const fileOwner = await User.findOne({
-    $or: [
-      { "uploadedSongs.audioFileName": filename },
-      { "uploadedSongs.imageFileName": filename },
-    ],
-  });
-  const isOwner = sender.id === fileOwner.id;
-  return isOwner;
+  try {
+    // Find a user who owns the file, either audio or image
+    const fileOwner = await User.findOne({
+      $or: [
+        { "uploadedSongs.audioFileName": filename },
+      ],
+    });
+
+    if (!fileOwner) {
+      throw new Error("File owner not found");
+    }
+
+    console.log(sender.id === fileOwner.id);
+    
+
+    return sender.id === fileOwner.id;
+  } catch (error) {
+    console.error("Ownership verification failed:", error.message);
+    throw new Error(`Ownership verification failed: ${error.message}`);
+  }
 };
 
 router.get("/:filename", accessControl("files", "readOwn", isOwner), getFile);
 router.delete(
-  "/:fileNames",
+  "/:filename",
   accessControl("files", "deleteOwn", isOwner),
   deleteFile
 );
